645f48fd73fa62dfc45cf691f4f974fce706a0cc
Huy Pham <huypt110402@gmail.com>
2024-04-04 00:24:09 +0700

    base code

---
+++ src/main/java/com/example/rosaceae/RosaceaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rosaceae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosaceaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosaceaeApplication.class, args);
+	}
+
+}
