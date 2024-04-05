3993ee569701c88a88375e5f92cc58a8e59c2fc6
Huy Pham <huypt110402@gmail.com>
2024-04-05 12:48:27 +0700

    add model user + token

---
+++ src/main/java/com/example/rosaceae/auth/AuthenticationResponse.java
@@ -0,0 +1,21 @@
+package com.example.rosaceae.auth;
+
+import com.example.rosaceae.model.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    private String status;
+    private User userInfo;
+}
