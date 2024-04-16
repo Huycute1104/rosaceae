e489df3fde981cd648e23baeb8772286d43a8e26
Huy Pham <huypt110402@gmail.com>
2024-04-16 18:30:28 +0700

    add model ItemTpye

---
+++ src/main/java/com/example/rosaceae/model/ItemType.java
@@ -0,0 +1,23 @@
+package com.example.rosaceae.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ItemType")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ItemType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer itemTypeId;
+
+    @Column(name = "ItemTypeName", length = 30)
+    private String itemTypeName;
+}
