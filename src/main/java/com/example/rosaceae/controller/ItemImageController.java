99a6404a4cd060efaaa510b0d049dae938917a93
Huy Pham <huypt110402@gmail.com>
2024-05-19 23:02:56 +0700

    add api[delete ItemImages]

--- src/main/java/com/example/rosaceae/controller/ItemImageController.java
+++ src/main/java/com/example/rosaceae/controller/ItemImageController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -36,5 +33,12 @@ public class ItemImageController {
 
         ItemImageResponse response = itemImageService.CreateItemImage(request);
         return ResponseEntity.ok(response);
+    }@DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('shop:delete')")
+    public ResponseEntity<ItemImageResponse> deleteItem(@PathVariable int id) {
+        ItemImageResponse response = itemImageService.DeleteItemImage(id);
+        return ResponseEntity.ok(response);
     }
+
+
 }
