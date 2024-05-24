c63c217a44c558336b6a31b688a5f39739d3fef4
Huy Pham <huypt110402@gmail.com>
2024-05-24 11:40:42 +0700

    change

--- src/main/java/com/example/rosaceae/RosaceaeApplication.java
+++ src/main/java/com/example/rosaceae/RosaceaeApplication.java
@@ -97,159 +97,159 @@ public class RosaceaeApplication {
     }
 
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner(
-//			AuthenticationService service,
-//			RankMemberService memberService,
-//			CategoryService categoryService,
-//			ItemTypeService itemTypeService,
-//			ItemService itemService
-//	) {
-//		return args -> {
-//			// dummy data rank
-//			var rank = CreateRankRequet.builder()
-//					.rankName("Bronze")
-//					.build();
-//			var bronze = memberService.createRank(rank);
-//
-//			var rank2 = CreateRankRequet.builder()
-//					.rankName("Silver")
-//					.build();
-//			memberService.createRank(rank2);
-//			var rank3 = CreateRankRequet.builder()
-//					.rankName("Gold")
-//					.build();
-//			memberService.createRank(rank3);
-//			//dummy data user
-//			//Supper admin
-//			var super_admin = RegisterRequest.builder()
-//					.name("SuperAdmin")
-//					.email("SuperAdmin@gmail.com")
-//					.password("123")
-//					.phone("0392272536")
-//					.status(true)
-//					.role(SUPPER_ADMIN)
-//					.enabled(true)
-//					.build();
-//			System.out.println("Super Admin token :" + service.register(super_admin).getAccessToken());
-//
-//			//admin
-//			var admin = RegisterRequest.builder()
-//					.name("Admin")
-//					.email("admin@gmail.com")
-//					.status(true)
-//					.password("123")
-//					.phone("0854512367")
-//					.role(ADMIN)
-//					.enabled(true)
-//					.build();
-//			System.out.println("Admin token :" + service.register(admin).getAccessToken());
-//
-//			//shop
-//			var shop = RegisterRequest.builder()
-//					.name("Rosaceae Shop")
-//					.email("rosaceae001@gmail.com")
-//					.status(true)
-//					.password("123")
-//					.phone("0854512367")
-//					.role(SHOP)
-//					.enabled(true)
-//					.build();
-//			System.out.println("Shop token :" + service.register(shop).getAccessToken());
-//
-//			//Customer
-//			var customer = RegisterRequest.builder()
-//					.name("Trần Huy")
-//					.email("huypt110402@gmail.com")
-//					.status(true)
-//					.password("123")
-//					.phone("0854512367")
-//					.role(CUSTOMER)
-//					.rankId(1)
-//					.enabled(true)
-//					.build();
-//			System.out.println("Customer token :" + service.register(customer).getAccessToken());
-//
-//			var customer2 = RegisterRequest.builder()
-//					.name("Huyền Trân")
-//					.email("tran123@gmail.com")
-//					.status(true)
-//					.password("123")
-//					.phone("0854512367")
-//					.role(CUSTOMER)
-//					.rankId(1)
-//					.enabled(true)
-//					.build();
-//			service.register(customer2);
-//
-//			//dummy data category
-//			var category = CreateCategoryRequest.builder()
-//					.categoryName("Facial")
-//					.build();
-//			categoryService.createCategory(category);
-//
-//			var category1 = CreateCategoryRequest.builder()
-//					.categoryName("BodyCare")
-//					.build();
-//			categoryService.createCategory(category1);
-//
-//			var category2 = CreateCategoryRequest.builder()
-//					.categoryName("Nail")
-//					.build();
-//			categoryService.createCategory(category2);
-//
-//			var category3 = CreateCategoryRequest.builder()
-//					.categoryName("HairCare")
-//					.build();
-//			categoryService.createCategory(category3);
-//			var category4 = CreateCategoryRequest.builder()
-//					.categoryName("Relaxation")
-//					.build();
-//			categoryService.createCategory(category4);
-//
-//			// dummy data itemType
-//			var itemType = ItemTypeRequest.builder()
-//					.name("Service")
-//					.build();
-//			itemTypeService.createItemType(itemType);
-//			var itemType1 = ItemTypeRequest.builder()
-//					.name("Product")
-//					.build();
-//			itemTypeService.createItemType(itemType1);
-//
-//			// dummy data Item
-//			//service
-//			var item = ItemRequest.builder()
-//					.itemName("Service")
-//					.quantity(0)
-//					.price(10f)
-//					.description("hihi")
-////					.commentCount(0)
-////					.rate(0f)
-////					.countUsage(0)
-//					.shopId(3)
-//					.categoryId(1)
-//					.itemTypeId(1)
-//					.discount(10)
-//					.build();
-//			itemService.CreateItem(item);
-//			//product
-//			var item1 = ItemRequest.builder()
-//					.itemName("Product")
-//					.quantity(0)
-//					.price(10f)
-//					.description("hihi")
-////					.commentCount(0)
-////					.rate(0f)
-////					.countUsage(0)
-//					.shopId(3)
-//					.categoryId(1)
-//					.itemTypeId(1)
-//					.discount(10)
-//					.build();
-//			itemService.CreateItem(item1);
-//
-//
-//		};
-//	}
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service,
+			RankMemberService memberService,
+			CategoryService categoryService,
+			ItemTypeService itemTypeService,
+			ItemService itemService
+	) {
+		return args -> {
+			// dummy data rank
+			var rank = CreateRankRequet.builder()
+					.rankName("Bronze")
+					.build();
+			var bronze = memberService.createRank(rank);
+
+			var rank2 = CreateRankRequet.builder()
+					.rankName("Silver")
+					.build();
+			memberService.createRank(rank2);
+			var rank3 = CreateRankRequet.builder()
+					.rankName("Gold")
+					.build();
+			memberService.createRank(rank3);
+			//dummy data user
+			//Supper admin
+			var super_admin = RegisterRequest.builder()
+					.name("SuperAdmin")
+					.email("SuperAdmin@gmail.com")
+					.password("123")
+					.phone("0392272536")
+					.status(true)
+					.role(SUPPER_ADMIN)
+					.enabled(true)
+					.build();
+			System.out.println("Super Admin token :" + service.register(super_admin).getAccessToken());
+
+			//admin
+			var admin = RegisterRequest.builder()
+					.name("Admin")
+					.email("admin@gmail.com")
+					.status(true)
+					.password("123")
+					.phone("0854512367")
+					.role(ADMIN)
+					.enabled(true)
+					.build();
+			System.out.println("Admin token :" + service.register(admin).getAccessToken());
+
+			//shop
+			var shop = RegisterRequest.builder()
+					.name("Rosaceae Shop")
+					.email("rosaceae001@gmail.com")
+					.status(true)
+					.password("123")
+					.phone("0854512367")
+					.role(SHOP)
+					.enabled(true)
+					.build();
+			System.out.println("Shop token :" + service.register(shop).getAccessToken());
+
+			//Customer
+			var customer = RegisterRequest.builder()
+					.name("Trần Huy")
+					.email("huypt110402@gmail.com")
+					.status(true)
+					.password("123")
+					.phone("0854512367")
+					.role(CUSTOMER)
+					.rankId(1)
+					.enabled(true)
+					.build();
+			System.out.println("Customer token :" + service.register(customer).getAccessToken());
+
+			var customer2 = RegisterRequest.builder()
+					.name("Huyền Trân")
+					.email("tran123@gmail.com")
+					.status(true)
+					.password("123")
+					.phone("0854512367")
+					.role(CUSTOMER)
+					.rankId(1)
+					.enabled(true)
+					.build();
+			service.register(customer2);
+
+			//dummy data category
+			var category = CreateCategoryRequest.builder()
+					.categoryName("Facial")
+					.build();
+			categoryService.createCategory(category);
+
+			var category1 = CreateCategoryRequest.builder()
+					.categoryName("BodyCare")
+					.build();
+			categoryService.createCategory(category1);
+
+			var category2 = CreateCategoryRequest.builder()
+					.categoryName("Nail")
+					.build();
+			categoryService.createCategory(category2);
+
+			var category3 = CreateCategoryRequest.builder()
+					.categoryName("HairCare")
+					.build();
+			categoryService.createCategory(category3);
+			var category4 = CreateCategoryRequest.builder()
+					.categoryName("Relaxation")
+					.build();
+			categoryService.createCategory(category4);
+
+			// dummy data itemType
+			var itemType = ItemTypeRequest.builder()
+					.name("Service")
+					.build();
+			itemTypeService.createItemType(itemType);
+			var itemType1 = ItemTypeRequest.builder()
+					.name("Product")
+					.build();
+			itemTypeService.createItemType(itemType1);
+
+			// dummy data Item
+			//service
+			var item = ItemRequest.builder()
+					.itemName("Service")
+					.quantity(0)
+					.price(10f)
+					.description("hihi")
+//					.commentCount(0)
+//					.rate(0f)
+//					.countUsage(0)
+					.shopId(3)
+					.categoryId(1)
+					.itemTypeId(1)
+					.discount(10)
+					.build();
+			itemService.CreateItem(item);
+			//product
+			var item1 = ItemRequest.builder()
+					.itemName("Product")
+					.quantity(0)
+					.price(10f)
+					.description("hihi")
+//					.commentCount(0)
+//					.rate(0f)
+//					.countUsage(0)
+					.shopId(3)
+					.categoryId(1)
+					.itemTypeId(1)
+					.discount(10)
+					.build();
+			itemService.CreateItem(item1);
+
+
+		};
+	}
 }
