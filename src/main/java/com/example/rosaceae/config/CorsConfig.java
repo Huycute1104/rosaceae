ff0248074d56dd9ebd535c6fba41254a85bd60f7
HuyPham <huyptse160548@fpt.edu.vn>
2024-06-28 10:27:22 +0700

    config port

--- src/main/java/com/example/rosaceae/config/CorsConfig.java
+++ src/main/java/com/example/rosaceae/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000",
                         "http://localhost:5173",
-                        "https://21bc-42-117-186-213.ngrok-free.app",
+                        "https://80a9-42-117-186-213.ngrok-free.app",
                         "https://rosaceae.id.vn",
                         "https://rosaceae-admin.vercel.app",
                         "https://rosaceae.vercel.app")
